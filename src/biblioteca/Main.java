67d1a2462e7b723b05119ed0ae44a7042a98a7c0
MIRKO <mirko.cuter@spinelli-dida.local>
2024-05-08 14:19:25 +0200

    :(

--- src/biblioteca/Main.java
+++ src/biblioteca/Main.java
@@ -18,9 +18,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main  {
+public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ciao, benvenuto nella biblioteca, cosa vuoi fare?");
+
+        String scelta = scanner.nextLine();
         try {
             // Caricamento del documento XML esistente
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -36,23 +40,15 @@ public class Main  {
                 doc.appendChild(doc.createElement("libri"));
             }
 
-            // Input dei nuovi dati dall'utente
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Inserisci i titoli dei libri da aggiungere: ");
+        switch (scelta) {
+            case "add" ->
+                System.out.println("add");
+            case "del" ->{
+                System.out.println("quanti libri vuoi eliminare? ");
+                int n=scanner.nextInt();
+               
 
-            for (int i = 0; i < 3; i++) {
-                System.out.print("Titolo del libro " + (i + 1) + ": ");
-                String titoloLibro = scanner.nextLine();
-
-                Element libroElement = doc.createElement("libro");
-                libroElement.setAttribute("titolo", titoloLibro); // Aggiunta dell'attributo titolo
-                doc.getDocumentElement().appendChild(libroElement);
-            }
-
-            // Richiesta dei titoli dei libri da eliminare
-            System.out.println("Inserisci i titoli dei libri da eliminare: ");
-
-            for (int i = 0; i < 2; i++) {
+                for (int i = 0; i < n; i++) {
                 System.out.print("Titolo del libro da eliminare " + (i + 1) + ": ");
                 String titoloDaEliminare = scanner.nextLine();
 
@@ -69,30 +65,89 @@ public class Main  {
                         }
                     }
                 }
-            }
-
-            // Salva il documento XML aggiornato su un file
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(file);
-            transformer.transform(source, result);
-            System.out.println("File XML aggiornato con successo!");
-
-            // Lettura del file XML aggiornato
-            System.out.println("Titoli dei libri nel file XML:");
-            NodeList libroNodes = doc.getElementsByTagName("libro");
-
-            for (int i = 0; i < libroNodes.getLength(); i++) {
-                Node libroNode = libroNodes.item(i);
-                if (libroNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element libroElement = (Element) libroNode;
-                    String titoloLibro = libroElement.getAttribute("titolo"); // Ottenere l'attributo titolo
-                    System.out.println("- " + titoloLibro);
-                }
-            }
-        } catch (ParserConfigurationException | SAXException | IOException | TransformerException e) {
-            e.printStackTrace();
+            }}
+            case "prestito" ->
+                System.out.println("prestito");
+            case "restituzione" ->
+                System.out.println("restituzione");
+            case "storico" ->
+                System.out.println("storico");
         }
+          } catch (ParserConfigurationException | SAXException | IOException  e) {
+        }
+
+//////////////////////////////////////        try {
+//////////////////////////////////////            // Caricamento del documento XML esistente
+//////////////////////////////////////            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+//////////////////////////////////////            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+//////////////////////////////////////            Document doc;
+//////////////////////////////////////
+//////////////////////////////////////            File file = new File("src\\biblioteca\\libri.xml");
+//////////////////////////////////////            if (file.exists()) {
+//////////////////////////////////////                doc = dBuilder.parse(file);
+//////////////////////////////////////                doc.getDocumentElement().normalize();
+//////////////////////////////////////            } else {
+//////////////////////////////////////                doc = dBuilder.newDocument();
+//////////////////////////////////////                doc.appendChild(doc.createElement("libri"));
+//////////////////////////////////////            }
+//////////////////////////////////////
+//            // Input dei nuovi dati dall'utente
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Inserisci i titoli dei libri da aggiungere: ");
+//
+//            for (int i = 0; i < 3; i++) {
+//                System.out.print("Titolo del libro " + (i + 1) + ": ");
+//                String titoloLibro = scanner.nextLine();
+//
+//                Element libroElement = doc.createElement("libro");
+//                libroElement.setAttribute("titolo", titoloLibro); // Aggiunta dell'attributo titolo
+//                doc.getDocumentElement().appendChild(libroElement);
+//            }
+//
+//            // Richiesta dei titoli dei libri da eliminare
+//            System.out.println("Inserisci i titoli dei libri da eliminare: ");
+//
+//            for (int i = 0; i < 2; i++) {
+//                System.out.print("Titolo del libro da eliminare " + (i + 1) + ": ");
+//                String titoloDaEliminare = scanner.nextLine();
+//
+//                NodeList libroNodes = doc.getElementsByTagName("libro");
+//
+//                for (int j = 0; j < libroNodes.getLength(); j++) {
+//                    Node libroNode = libroNodes.item(j);
+//                    if (libroNode.getNodeType() == Node.ELEMENT_NODE) {
+//                        Element libroElement = (Element) libroNode;
+//                        String titoloLibro = libroElement.getAttribute("titolo"); // Ottenere l'attributo titolo
+//                        if (titoloLibro.equals(titoloDaEliminare)) {
+//                            libroElement.getParentNode().removeChild(libroElement);
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//
+//            // Salva il documento XML aggiornato su un file
+//            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+//            Transformer transformer = transformerFactory.newTransformer();
+//            DOMSource source = new DOMSource(doc);
+//            StreamResult result = new StreamResult(file);
+//            transformer.transform(source, result);
+//            System.out.println("File XML aggiornato con successo!");
+//
+//            // Lettura del file XML aggiornato
+//            System.out.println("Titoli dei libri nel file XML:");
+//            NodeList libroNodes = doc.getElementsByTagName("libro");
+//
+//            for (int i = 0; i < libroNodes.getLength(); i++) {
+//                Node libroNode = libroNodes.item(i);
+//                if (libroNode.getNodeType() == Node.ELEMENT_NODE) {
+//                    Element libroElement = (Element) libroNode;
+//                    String titoloLibro = libroElement.getAttribute("titolo"); // Ottenere l'attributo titolo
+//                    System.out.println("- " + titoloLibro);
+//                }
+//            }
+//        } catch (ParserConfigurationException | SAXException | IOException | TransformerException e) {
+//            e.printStackTrace();
+//        }
     }
 }
