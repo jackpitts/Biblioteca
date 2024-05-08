7bedc316f17d7fdfab10d36b6f3aaf9e7a4b76ee
JACOPO <jacopo.careddu@spinelli-dida.local>
2024-05-08 13:52:58 +0200

    fixato un botto di roba

--- src/biblioteca/Libro.java
+++ src/biblioteca/Libro.java
@@ -1,33 +1,29 @@
 package biblioteca;
 
+import java.util.Date;
+
 class Libro {
+
     private String titolo, autore, casaEditrice;
     private int anno;
     private boolean inPrestito;
+    private Date data;
 
-
-    public Libro(String titolo, String autore, String casaEditrice, int anno) {
+    public Libro(String titolo, String autore, String casaEditrice, int anno, boolean inPrestito, Date data) {
         this.titolo = titolo;
         this.autore = autore;
         this.casaEditrice = casaEditrice;
         this.anno = anno;
-        this.inPrestito = false;
+        this.inPrestito = inPrestito;
+        this.data = data;
     }
 
     public String getTitolo() {
         return titolo;
     }
 
-    public boolean isInPrestito() {
-        return inPrestito;
-    }
-
-    public void prendiInPrestito() {
-        inPrestito = true;
-    }
-
-    public void restituisci() {
-        inPrestito = false;
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
     }
 
     public String getAutore() {
@@ -54,4 +50,27 @@ class Libro {
         this.anno = anno;
     }
 
+    public boolean isInPrestito() {
+        return inPrestito;
+    }
+
+    public void setInPrestito(boolean inPrestito) {
+        this.inPrestito = inPrestito;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public void prendiInPrestito() {
+        inPrestito = true;
+    }
+
+    public void restituisci() {
+        inPrestito = false;
+    }
 }
--- src/biblioteca/Persona.java
+++ src/biblioteca/Persona.java
@@ -1,13 +1,30 @@
 package biblioteca;
 
 class Persona {
-    private String nome;
 
-    public Persona(String nome) {
+    private String nome;
+    private String email;
+    private String password;
+
+    public Persona(String nome, String email, String password) {
         this.nome = nome;
+        this.email = email;
+        this.password = password;
     }
 
-    public String getNome() {
-        return nome;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
