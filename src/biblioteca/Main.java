f4a3f0357f9326d752868d42931c202021029546
JACOPO <jacopo.careddu@spinelli-dida.local>
2024-05-08 14:28:16 +0200

    del

--- src/biblioteca/Main.java
+++ src/biblioteca/Main.java
@@ -1,153 +1,63 @@
 package biblioteca;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
+import javax.xml.transform.TransformerException;
 
 public class Main {
 
     public static void main(String[] args) {
+        XMLArchive archive = new XMLArchive();
         Scanner scanner = new Scanner(System.in);
         System.out.println("ciao, benvenuto nella biblioteca, cosa vuoi fare?");
 
         String scelta = scanner.nextLine();
-        try {
-            // Caricamento del documento XML esistente
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc;
-
-            File file = new File("src\\biblioteca\\libri.xml");
-            if (file.exists()) {
-                doc = dBuilder.parse(file);
-                doc.getDocumentElement().normalize();
-            } else {
-                doc = dBuilder.newDocument();
-                doc.appendChild(doc.createElement("libri"));
-            }
-
         switch (scelta) {
-            case "add" ->
-                System.out.println("add");
-            case "del" ->{
-                System.out.println("quanti libri vuoi eliminare? ");
-                int n=scanner.nextInt();
-               
+            case "add" -> {
+                // Aggiungi un libro all'archivio
+            }
+            case "del" -> {
+                System.out.println("Quanti libri vuoi eliminare?");
+                int n = scanner.nextInt();
+                scanner.nextLine(); // Consuma il carattere di nuova riga residuo
 
                 for (int i = 0; i < n; i++) {
-                System.out.print("Titolo del libro da eliminare " + (i + 1) + ": ");
-                String titoloDaEliminare = scanner.nextLine();
-
-                NodeList libroNodes = doc.getElementsByTagName("libro");
-
-                for (int j = 0; j < libroNodes.getLength(); j++) {
-                    Node libroNode = libroNodes.item(j);
-                    if (libroNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element libroElement = (Element) libroNode;
-                        String titoloLibro = libroElement.getAttribute("titolo"); // Ottenere l'attributo titolo
-                        if (titoloLibro.equals(titoloDaEliminare)) {
-                            libroElement.getParentNode().removeChild(libroElement);
-                            break;
+                    System.out.print("Titolo del libro da eliminare " + (i + 1) + ": ");
+                    String titoloDaEliminare = scanner.nextLine();
+                    if (eliminaLibro(archive, titoloDaEliminare)) {
+                        try {
+                            archive.makeArchivePersistent();
+                            System.out.println("Libro eliminato con successo");
+                        } catch (TransformerException ex) {
+                            System.err.println("Errore durante la scrittura del file XML: " + ex.getMessage());
                         }
+                    } else {
+                        System.out.println("Libro non trovato: " + titoloDaEliminare);
                     }
                 }
-            }}
-            case "prestito" ->
-                System.out.println("prestito");
-            case "restituzione" ->
-                System.out.println("restituzione");
-            case "storico" ->
-                System.out.println("storico");
+            }
+            case "prestito" -> {
+                // Operazioni per prestito
+            }
+            case "restituzione" -> {
+                // Operazioni per restituzione
+            }
+            case "storico" -> {
+                // Operazioni per visualizzare lo storico
+            }
+            default -> System.out.println("Comando non riconosciuto");
         }
-          } catch (ParserConfigurationException | SAXException | IOException  e) {
-        }
+    }
 
-//////////////////////////////////////        try {
-//////////////////////////////////////            // Caricamento del documento XML esistente
-//////////////////////////////////////            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-//////////////////////////////////////            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-//////////////////////////////////////            Document doc;
-//////////////////////////////////////
-//////////////////////////////////////            File file = new File("src\\biblioteca\\libri.xml");
-//////////////////////////////////////            if (file.exists()) {
-//////////////////////////////////////                doc = dBuilder.parse(file);
-//////////////////////////////////////                doc.getDocumentElement().normalize();
-//////////////////////////////////////            } else {
-//////////////////////////////////////                doc = dBuilder.newDocument();
-//////////////////////////////////////                doc.appendChild(doc.createElement("libri"));
-//////////////////////////////////////            }
-//////////////////////////////////////
-//            // Input dei nuovi dati dall'utente
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("Inserisci i titoli dei libri da aggiungere: ");
-//
-//            for (int i = 0; i < 3; i++) {
-//                System.out.print("Titolo del libro " + (i + 1) + ": ");
-//                String titoloLibro = scanner.nextLine();
-//
-//                Element libroElement = doc.createElement("libro");
-//                libroElement.setAttribute("titolo", titoloLibro); // Aggiunta dell'attributo titolo
-//                doc.getDocumentElement().appendChild(libroElement);
-//            }
-//
-//            // Richiesta dei titoli dei libri da eliminare
-//            System.out.println("Inserisci i titoli dei libri da eliminare: ");
-//
-//            for (int i = 0; i < 2; i++) {
-//                System.out.print("Titolo del libro da eliminare " + (i + 1) + ": ");
-//                String titoloDaEliminare = scanner.nextLine();
-//
-//                NodeList libroNodes = doc.getElementsByTagName("libro");
-//
-//                for (int j = 0; j < libroNodes.getLength(); j++) {
-//                    Node libroNode = libroNodes.item(j);
-//                    if (libroNode.getNodeType() == Node.ELEMENT_NODE) {
-//                        Element libroElement = (Element) libroNode;
-//                        String titoloLibro = libroElement.getAttribute("titolo"); // Ottenere l'attributo titolo
-//                        if (titoloLibro.equals(titoloDaEliminare)) {
-//                            libroElement.getParentNode().removeChild(libroElement);
-//                            break;
-//                        }
-//                    }
-//                }
-//            }
-//
-//            // Salva il documento XML aggiornato su un file
-//            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-//            Transformer transformer = transformerFactory.newTransformer();
-//            DOMSource source = new DOMSource(doc);
-//            StreamResult result = new StreamResult(file);
-//            transformer.transform(source, result);
-//            System.out.println("File XML aggiornato con successo!");
-//
-//            // Lettura del file XML aggiornato
-//            System.out.println("Titoli dei libri nel file XML:");
-//            NodeList libroNodes = doc.getElementsByTagName("libro");
-//
-//            for (int i = 0; i < libroNodes.getLength(); i++) {
-//                Node libroNode = libroNodes.item(i);
-//                if (libroNode.getNodeType() == Node.ELEMENT_NODE) {
-//                    Element libroElement = (Element) libroNode;
-//                    String titoloLibro = libroElement.getAttribute("titolo"); // Ottenere l'attributo titolo
-//                    System.out.println("- " + titoloLibro);
-//                }
-//            }
-//        } catch (ParserConfigurationException | SAXException | IOException | TransformerException e) {
-//            e.printStackTrace();
-//        }
+    private static boolean eliminaLibro(XMLArchive archive, String titolo) {
+        var doc = archive.getDocument();
+        var libroNodes = doc.getElementsByTagName("libro");
+        for (int i = 0; i < libroNodes.getLength(); i++) {
+            var libroElement = (org.w3c.dom.Element) libroNodes.item(i);
+            if (libroElement.getAttribute("titolo").equals(titolo)) {
+                libroElement.getParentNode().removeChild(libroElement);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/biblioteca/XMLArchive.java
+++ src/biblioteca/XMLArchive.java
@@ -1,87 +1,64 @@
 package biblioteca;
 
-import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import java.io.File;
+import java.io.IOException;
 
 public class XMLArchive {
 
     private static final String ARCHIVE_PATH = "src\\biblioteca\\libri.xml";
-    DocumentBuilderFactory dbFactory;
-    DocumentBuilder dBuilder;
-    Document doc;
-    File file;
+    private Document doc;
 
     public XMLArchive() {
         try {
-            dbFactory = DocumentBuilderFactory.newInstance();
-            dBuilder = dbFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(XMLArchive.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public void createArchive() {
-        file = new File(ARCHIVE_PATH);
-        if (file.exists()) {
-            try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            File file = new File(ARCHIVE_PATH);
+            if (file.exists()) {
                 doc = dBuilder.parse(file);
                 doc.getDocumentElement().normalize();
-            } catch (Exception ex) {
-                Logger.getLogger(XMLArchive.class.getName()).log(Level.SEVERE, null, ex);
+            } else {
+                doc = dBuilder.newDocument();
+                doc.appendChild(doc.createElement("libri"));
             }
-        } else {
-            doc = dBuilder.newDocument();
-            doc.appendChild(doc.createElement("libri"));
+        } catch (ParserConfigurationException | IOException | org.xml.sax.SAXException e) {
+            e.printStackTrace();
         }
     }
 
-    public void addBook(Libro libro) {
-        Element libroElement = doc.createElement("libro");
-        libroElement.setAttribute("titolo", libro.getTitolo()); 
-        libroElement.setAttribute("autore", libro.getTitolo()); 
-        libroElement.setAttribute("casaEditrice", libro.getTitolo()); 
-        libroElement.setAttribute("anno", libro.getTitolo()); 
-        doc.getDocumentElement().appendChild(libroElement);
+    public Document getDocument() {
+        return doc;
     }
 
-    public void makeArchivePersistent() {
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer;
+    public void makeArchivePersistent() throws TransformerException {
         try {
-            transformer = transformerFactory.newTransformer();
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(file);
+            StreamResult result = new StreamResult(new File(ARCHIVE_PATH));
             transformer.transform(source, result);
-        } catch (TransformerConfigurationException ex) {
-            Logger.getLogger(XMLArchive.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (TransformerException ex) {
-            Logger.getLogger(XMLArchive.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+            throw e;
         }
     }
 
-    public void readArchive() {        
+    public void readArchive() {
         NodeList libroNodes = doc.getElementsByTagName("libro");
         for (int i = 0; i < libroNodes.getLength(); i++) {
-            Node libroNode = libroNodes.item(i);
-            if (libroNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element libroElement = (Element) libroNode;
-                String titoloLibro = libroElement.getAttribute("titolo"); 
-                System.out.println("- " + titoloLibro);
-            }
+            Element libroElement = (Element) libroNodes.item(i);
+            String titoloLibro = libroElement.getAttribute("titolo");
+            System.out.println("- " + titoloLibro);
         }
     }
 }
